baa2bc53acd38c173876e14484f97a63af2f42f1
Jason Randolph Eads <jeads442@gmail.com>
2020-07-24 21:14:04 -0500

    Added messages to asserts in unit tests.

--- code/node/pgdocTest.js
+++ code/node/pgdocTest.js
@@ -36,45 +36,46 @@ let testBasic = async () => {
   let connectionString = config.connectionString
   let options = { verbose: config.verbose }
   let rv = await pgdoc.connect( { connectionString, options } )
-  assert( !rv.error )
+  assert( !rv.error, `connect failed with error ${str(rv)}` )
   rl.write(`passed.\n`)
 
   rl.write(`configure()...                                                 `)
   rv = await pgdoc.configure( { options: { verbose: config.verbose, quiet: config.quiet } } )
-  assert( !rv.error )
+  assert( !rv.error, `configure failed with error ${str(rv)}` )
   rl.write(`passed.\n`)
 
   let type = `pgdocTest`
 
   rl.write(`requestID()...                                                 `)
   rv = await pgdoc.requestID( { type } )
-  assert( !rv.error )
+  assert( !rv.error, `requestID failed with error ${str(rv)}` )
   let id = rv
   rl.write(`passed.\n  ID: ${id}\n`)
 
   rl.write(`ensuring no conflicting records in database via delete()...    `)
   rv = await pgdoc.delete( { type: `pgdocTest` } )
-  assert( !rv.error )
+  assert( !rv.error, `delete failed with error ${str(rv)}` )
   rl.write(`passed.\n  Deleted ${rv.deleted} documents.\n`)
 
   let doc = { id, x: 1, y: 2, z: 3 }
 
   rl.write(`store()...                                                     `)
   rv = await pgdoc.store( { type, doc } )
-  assert( !rv.error )
+  assert( !rv.error, `store failed with error ${str(rv)}` )
   rl.write(`passed.\n  Stored: ${str(doc)}\n`)
 
   let search = { id }
 
   rl.write(`retrieve()...                                                  `)
   rv = await pgdoc.retrieve( { type, search } )
-  assert( rv.length == 1 )
+  assert( !rv.error, `retrieve failed with error ${str(rv)}` )
+  assert( rv.length == 1, `retrieve failed to get expected result, expected 1 document and got ${rv.length}` )
   rl.write(`passed.\n  Retrieved: ${str(rv[0])}\n`)
 
   rl.write(`delete()...                                                    `)
   rv = await pgdoc.delete( { type, search } )
-  assert( !rv.error )
-  assert( rv.deleted == 1 )
+  assert( !rv.error, `delete failed with error ${str(rv)}` )
+  assert( rv.deleted == 1, `delete failed to get expected result, expected 1 deletion and got ${rv.deleted}` )
   rl.write(`passed.\n  Deleted ${rv.deleted} documents.\n`)
 
   rl.write(`Testing Basic Use Cases...                                     passed.\n\n`)
@@ -86,83 +87,83 @@ let testAdvancedStore = async () => {
   let connectionString = config.connectionString
   let options = { verbose: config.verbose }
   let rv = await pgdoc.connect( { connectionString, options } )
-  assert( !rv.error )
+  assert( !rv.error, `connect failed with error ${str(rv)}` )
 
   let type = `pgdocTest`
 
   rl.write(`ensuring no conflicting records in database via delete()...    `)
   rv = await pgdoc.delete( { type: `pgdocTest` } )
-  assert( !rv.error )
+  assert( !rv.error, `delete failed with error ${str(rv)}` )
   rl.write(`passed.\n  Deleted ${rv.deleted} documents.\n`)
 
   rl.write(`store() search test...                                         `)
   let oldDoc = { id: `-15`, v: 1 }
   rv = await pgdoc.store( { type, doc: oldDoc } )
-  assert( !rv.error )
-  assert( rv.deleted == 0 )
+  assert( !rv.error, `store of oldDoc failed with error ${str(rv)}` )
+  assert( rv.deleted == 0, `store failed to get expected result, expected 0 deletions and got ${rv.deleted}` )
   let newDoc = { id: `-15`, v: 2 }
   let search = { id: `-15` }
   rv = await pgdoc.store( { type, doc: newDoc, search } )
-  assert( !rv.error )
-  assert( rv.deleted == 1 )
+  assert( !rv.error, `store of newDoc failed with error ${str(rv)}` )
+  assert( rv.deleted == 1, `store failed to get expected result, expected 1 deletion and got ${rv.deleted}` )
   rl.write(`passed.\n  Stored: ${str(oldDoc)}\n  Updated: ${str(newDoc)}\n`)
 
   rl.write(`store() search + maxMatch test...                              `)
   oldDoc = { id: `-16`, v: 1 }
   rv = await pgdoc.store( { type, doc: oldDoc } )
-  assert( !rv.error )
-  assert( rv.deleted == 0 )
+  assert( !rv.error, `store of oldDoc failed with error ${str(rv)}` )
+  assert( rv.deleted == 0, `store failed to get expected result, expected 0 deletions and got ${rv.deleted}` )
   newDoc = { id: `-16`, v: 2 }
   search = { id: `-16` }
   let maxMatch = 1
   rv = await pgdoc.store( { type, doc: newDoc, search, maxMatch } )
-  assert( !rv.error )
-  assert( rv.deleted == 1 )
+  assert( !rv.error, `store of newDoc failed with error ${str(rv)}` )
+  assert( rv.deleted == 1, `store failed to get expected result, expected 1 deletion and got ${rv.deleted}` )
   rl.write(`passed.\n  Stored: ${str(oldDoc)}\n  Updated: ${str(newDoc)}\n`)
 
   rl.write(`store() search, exclude test...                                `)
   oldDoc = { id: `-17`, v: 1, ignore: false }
   rv = await pgdoc.store( { type, doc: oldDoc } )
-  assert( !rv.error )
-  assert( rv.deleted == 0 )
+  assert( !rv.error, `store of oldDoc failed with error ${str(rv)}` )
+  assert( rv.deleted == 0, `store failed to get expected result, expected 0 deletions and got ${rv.deleted}` )
   oldDoc2 = { id: `-17`, v: 1, ignore: true }
   rv = await pgdoc.store( { type, doc: oldDoc2 } )
-  assert( !rv.error )
-  assert( rv.deleted == 0 )
+  assert( !rv.error, `store of oldDoc2 failed with error ${str(rv)}` )
+  assert( rv.deleted == 0, `store failed to get expected result, expected 0 deletions and got ${rv.deleted}` )
   newDoc = { id: `-17`, v: 100, ignore: false }
   search = { id: `-17` }
   let exclude = { ignore: true }
   rv = await pgdoc.store( { type, doc: newDoc, search, exclude } )
-  assert( !rv.error )
-  assert( rv.deleted == 1 )
+  assert( !rv.error, `store of newDoc failed with error ${str(rv)}` )
+  assert( rv.deleted == 1, `store failed to get expected result, expected 1 deletion and got ${rv.deleted}` )
   let deleted = rv.deleted
   rv = await pgdoc.retrieve( { type, search } )
-  assert( rv.length == 2 )
+  assert( rv.length == 2, `retrieve failed to get expected result, expected 2 documents and got ${rv.length}` )
   rl.write(`passed.\n  Stored: ${str(oldDoc)}\n  Stored: ${str(oldDoc2)}\n  Updated: ${str(newDoc)}\n  Deleted: ${deleted}\n  Retrieved: ${str(rv[0])}\n  Retrieved: ${str(rv[1])}\n`)
 
 
   rl.write(`store() search + maxMatch, exclude test...                     `)
   oldDoc = { id: `-18`, v: 1, ignore: false }
   rv = await pgdoc.store( { type, doc: oldDoc } )
-  assert( !rv.error )
-  assert( rv.deleted == 0 )
+  assert( !rv.error, `store of oldDoc failed with error ${str(rv)}` )
+  assert( rv.deleted == 0, `store failed to get expected result, expected 0 deletions and got ${rv.deleted}` )
   oldDoc2 = { id: `-18`, v: 100, ignore: true }
   rv = await pgdoc.store( { type, doc: oldDoc2 } )
-  assert( !rv.error )
-  assert( rv.deleted == 0 )
+  assert( !rv.error, `store of oldDoc2 failed with error ${str(rv)}` )
+  assert( rv.deleted == 0, `store failed to get expected result, expected 0 deletions and got ${rv.deleted}` )
   newDoc = { id: `-18`, v: 2 }
   search = { id: `-18` }
   maxMatch = 1
   exclude = { ignore: true }
   rv = await pgdoc.store( { type, doc: newDoc, search, exclude, maxMatch } )
-  assert( !rv.error )
-  assert( rv.deleted == 1 )
+  assert( !rv.error, `store of newDoc failed with error ${str(rv)}` )
+  assert( rv.deleted == 1, `store failed to get expected result, expected 1 deletion and got ${rv.deleted}` )
   rl.write(`passed.\n  Stored: ${str(oldDoc)}\n  Stored: ${str(oldDoc2)}\n  Updated: ${str(newDoc)}\n  Deleted: ${rv.deleted}\n`)
 
   rl.write(`delete() to clean up...                                        `)
   rv = await pgdoc.delete( { type } )
-  assert( !rv.error )
-  assert( rv.deleted == 6 )
+  assert( !rv.error, `delete failed with error ${str(rv)}` )
+  assert( rv.deleted == 6, `delete failed to get expected result, expected 6 deletions and got ${rv.deleted}` )
   rl.write(`passed.\n  Deleted ${rv.deleted} documents.\n`)
 
   // console.error(rv)
